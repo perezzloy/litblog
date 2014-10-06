85a3f3460e1c23a0360459dd5be5ad8e5e1096a6
perezzloy <perezzloy@gmail.com>
2014-10-06 22:45:34 +0400

    Started the dev.
    We started the development of a front-end to a functional

--- www/index.php
+++ www/index.php
@@ -3,3 +3,63 @@
  * Автор: perezzloy
  * Создано: 05.10.2014 20:40
 */
+
+function getBlock($value){
+	$d = 'template/blocks/';
+	$e = '.php';
+	switch ($value) {
+		case '':
+			$getFile = $d.'main'.$e;
+			include $getFile;
+			echo $s;
+			break;
+
+		case 'main':
+			$getFile = $d.$value.$e;
+			include $getFile;
+			echo $s;
+			break;
+
+		case 'head':
+			$getFile = $d.$value.$e;
+			include $getFile;
+			echo $s;
+			break;
+
+		case 'header':
+			$getFile = $d.$value.$e;
+			include $getFile;
+			echo $s;
+			break;
+
+		case 'g-menu':
+			$getFile = $d.$value.$e;
+			include $getFile;
+			echo $s;
+			break;
+
+		case 'content':
+			$getFile = $d.$value.$e;
+			include $getFile;
+			echo $s;
+			break;
+
+		case 'footer':
+			$getFile = $d.$value.$e;
+			include $getFile;
+			echo $s;
+			break;
+		
+		default:
+			$getFile = $d.'404'.$e;
+			include $getFile;
+			echo $s;
+			break;
+	}
+}
+
+echo file_get_contents('template/s/main.html');
+
+function Templater($s){
+	
+}
---
+++ www/template/blocks/content.php
@@ -0,0 +1,4 @@
+<?php
+$s = '<div class="content">
+			Тут текст!!!1
+			</div>';
